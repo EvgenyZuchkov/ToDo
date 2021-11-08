ab6655d7fa20e55698ecd2dc6261355df2214403
Evgeny Zuchkov <jenyazuchkov@gmail.com>
2021-11-08 23:57:04 +0300

    Step 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './App.css';
 import TodoList from "./ToDo/TodoList";
 import Context from "./Context";
-import AddTodo from "./ToDo/AddTodo";
+import Loader from "./ToDo/Loader";
+
+// Technologies React lazy used for educational purposes
+const AddTodo = React.lazy(() => import ("./ToDo/AddTodo"))
 
 function App() {
 
     const [todos, setTodos] = React.useState([
-        {id: 1, completed: false, title: 'Купить молоко'},
-        {id: 2, completed: false, title: 'Купить хлеб'},
-        {id: 3, completed: false, title: 'Купить сахар'},
+        // {id: 1, completed: false, title: 'Купить молоко'},
+        // {id: 2, completed: false, title: 'Купить хлеб'},
+        // {id: 3, completed: false, title: 'Купить сахар'},
     ])
 
+    const [loading, setLoading] = React.useState(true)
+
+    useEffect( () => {
+        fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+            .then(response => response.json())
+            .then(todos => {
+                setTimeout(()=> {
+                    setTodos(todos)
+                    setLoading(false)
+                },2000)
+
+            })
+    },[])
 
     function toggleToDo(id) {
         setTodos(
@@ -45,8 +61,13 @@ function App() {
         <Context.Provider value={{removeToDo}}>
             <div className='wrapper'>
                 <h1>React tutorial</h1>
-                <AddTodo onCreate={addTodo}/>
-                {todos.length ? <TodoList todos={todos} toggleToDo={toggleToDo}/> : 'No todos'}
+                <React.Suspense fallback={<p>Loading...</p>}>
+                    <AddTodo onCreate={addTodo}/>
+                </React.Suspense>
+
+                {loading && <Loader/>}
+                {todos.length ? <TodoList todos={todos} toggleToDo={toggleToDo}/> :
+                    loading ? null : 'No todos!'}
             </div>
         </Context.Provider>
     );
--- src/ToDo/AddTodo.js
+++ src/ToDo/AddTodo.js
@@ -1,29 +1,43 @@
 import React, {useState} from "react";
 import PropTypes from 'prop-types';
 
-const AddTodo = ({onCreate}) => {
+
+// Create custom hook
+function useInputValue (defaultValue = '') {
     const [value, setValue] = useState('')
 
-    function submitHandler (event) {
+    return {
+        bind: {
+            value,
+            onChange: event => setValue(event.target.value)
+        },
+        clear: () => setValue(''),
+        value: () => value
+    }
+}
+
+const AddTodo = ({onCreate}) => {
+    const input = useInputValue('')
+
+    function submitHandler(event) {
         event.preventDefault()
 
-        if (value.trim()) {
-            onCreate(value)
-            setValue('')
+        if (input.value().trim()) {
+            onCreate(input.value())
+            input.clear()
         }
     }
 
     return (
         <form onSubmit={submitHandler}>
-            <input value={value}
-                   onChange={event => setValue(event.target.value)}/>
+            <input {...input.bind}/>
             <button type='submit'>Add todo</button>
         </form>
     )
 }
 
-AddTodo.propTypes = {
-    onCreate: PropTypes.func.isRequired
-}
+// AddTodo.propTypes = {
+//     onCreate: PropTypes.func.isRequired
+// }
 
 export default AddTodo;
